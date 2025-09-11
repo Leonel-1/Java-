ebd4bb88e9307e22ccfb80129e75f14ab6bb290a
Leonel-1 <LeonelJ10@nycstudents.net>
2025-09-11 14:14:47 +0000

    block

---
+++ Lesson-03B-DiscussionSolution/Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+
+    System.out.println("leonel Jimenez \n 38th street ave queens");
+
+    System.out.println
+    
+    System.out.println
+    
+  }
+
+  
+}
